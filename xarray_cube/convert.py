f9ba08708d08e0eba0374b18b7510884c082fdf4
出町史夏 <demachifumika@MacbookAir.local>
2021-09-30 16:35:47 +0900

    #6 Add conversion module

---
+++ xarray_cube/convert.py
@@ -0,0 +1,12 @@
+import xarray as xr
+import spectral_cube as sc
+
+
+def spectralcube2xarray(cube: sc.SpectralCube) -> xr.DataArray:
+    """Convert SpectralCube to xarray object."""
+    pass
+
+
+def xarray2spectralcube(da: xr.DataArray) -> sc.SpectralCube:
+    """Convert xarray object to SpectralCube."""
+    pass
