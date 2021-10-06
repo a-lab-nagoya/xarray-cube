d8ce775a6a98b54deba0771ba14e3a5871f42362
出町史夏 <demachifumika@MacbookAir.local>
2021-10-06 13:51:37 +0900

    #6 Fix docstrings

--- xarray_cube/converter.py
+++ xarray_cube/converter.py
@@ -3,10 +3,10 @@ import xarray as xr
 
 
 def spectralcube2xarray(cube: sc.SpectralCube) -> xr.DataArray:
-    """Convert SpectralCube to xarray object."""
+    """Convert a SpectralCube to a DataArray object."""
     pass
 
 
 def xarray2spectralcube(da: xr.DataArray) -> sc.SpectralCube:
-    """Convert xarray object to SpectralCube."""
+    """Convert a DataArray object to a SpectralCube."""
     pass
