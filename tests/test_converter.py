f197aba0088f195fbfcd13341e63d0ba6d75c2bd
Takeru Nishioka <tnishioka@a.phys.nagoya-u.ac.jp>
2021-10-11 16:18:21 +0900

    #20 Define test functions

---
+++ tests/test_converter.py
@@ -0,0 +1,21 @@
+from pathlib import Path
+
+# from astropy.io import fits
+# from astropy.io.fits import hdu
+# import spectral_cube as sc
+
+# from xarray_cube import converter, Cube
+
+DATA_DIR = Path("data")
+
+# hdu = fits.open(DATA_DIR / "GMC-8.spw16.12CO.7m.cube.pbcor.fits")
+# expected_data_array = Cube(hdu.data, hdu.header.tostring())
+# expected_sc_cube = sc.SpectralCube(hdu)
+
+
+def test_header():
+    assert True
+
+
+def test_data():
+    assert True
