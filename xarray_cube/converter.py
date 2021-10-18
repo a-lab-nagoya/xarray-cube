985473bebd023c22b31779d7a26184123e9c9623
r-yamada1998 <yamadier@icloud.com>
2021-10-18 17:59:19 +0900

    #19 spectralcube2xarray first commit

--- xarray_cube/converter.py
+++ xarray_cube/converter.py
@@ -1,12 +1,20 @@
 import spectral_cube as sc
 import xarray as xr
+from .cube import Cube
 
 
 def spectralcube2xarray(cube: sc.SpectralCube) -> xr.DataArray:
     """Convert a SpectralCube to a DataArray object."""
-    pass
+
+    DataCube = Cube.new(
+        data=cube.hdu.data,
+        header=cube.hdu.header,
+        Name=cube.hdu.header["BTYPE"],
+        units=cube.hdu.header["BUNIT"],
+    )
+    return DataCube
 
 
-def xarray2spectralcube(da: xr.DataArray) -> sc.SpectralCube:
+def xarray2spectralcube(data: xr.DataArray) -> sc.SpectralCube:
     """Convert a DataArray object to a SpectralCube."""
     pass
