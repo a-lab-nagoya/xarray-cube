bf442e6e8455ce02e8f0278c82f73831f783098f
Takeru Nishioka <tnishioka@a.phys.nagoya-u.ac.jp>
2021-10-25 11:44:44 +0900

    #20 Update test code of converter module

--- tests/test_converter.py
+++ tests/test_converter.py
@@ -1,16 +1,15 @@
 from pathlib import Path
 
-# from astropy.io import fits
-# from astropy.io.fits import hdu
-# import spectral_cube as sc
+from astropy.io import fits
+import spectral_cube as sc
 
-# from xarray_cube import converter, Cube
+from xarray_cube import converter, Cube
 
 DATA_DIR = Path("data")
 
-# hdu = fits.open(DATA_DIR / "GMC-8.spw16.12CO.7m.cube.pbcor.fits")
-# expected_data_array = Cube(hdu.data, hdu.header.tostring())
-# expected_sc_cube = sc.SpectralCube(hdu)
+hdu = fits.open(DATA_DIR / "GMC-8.spw16.12CO.7m.cube.pbcor.fits")
+expected_data_array = Cube(hdu.data, hdu.header.tostring())
+expected_sc_cube = sc.SpectralCube(hdu)
 
 
 def test_header():
