3e8c80393898d0662f96feeace0b8cbf7e4a7f9d
Takeru Nishioka <tnishioka@a.phys.nagoya-u.ac.jp>
2022-01-18 13:52:59 +0900

    #31 Add to extract wcs

--- xarray_cube/accessor.py
+++ xarray_cube/accessor.py
@@ -1,5 +1,8 @@
 import xarray as xr
+import astropy
 from astropy.io import fits
+
+from . import converter
 
 
 @xr.register_dataarray_accessor("cube")
@@ -11,3 +14,8 @@ class CubeAccessor:
     def header(self) -> fits.header.Header:
         """Convert the header string to a header object."""
         return fits.Header.fromstring(self._obj.header.item())
+
+    @property
+    def wcs(self) -> astropy.wcs.wcs.WCS:
+        """Extract header"""
+        return converter.xarray2spectralcube(self._obj).wcs
